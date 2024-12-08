72bfab790a6ae20b736e36486fe1d94775dde5e6
C-ArenA_WSL <carlostata.ca@gmail.com>
2024-12-08 18:31:02 -0400

    fix: Realiza correcciones de estilo al código usando vendor/bin/pint (NO ES RELEVANTE)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,52 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $usuarios = User::all();
+
         return $usuarios;
     }
 
-    public function create(){
+    public function create()
+    {
         return view('create');
     }
 
-    public function store(Request $request){
-        $user = new User();
+    public function store(Request $request)
+    {
+        $user = new User;
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = bcrypt($request->input('password'));
         $user->save();
+
         return $user;
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $user = User::find($id);
+
         return $user;
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $user = User::find($id);
-        return view('edit')->with('user',$user);
+
+        return view('edit')->with('user', $user);
     }
 
-    public function update(Request $request,$id){
+    public function update(Request $request, $id)
+    {
         $user = User::find($id);
         $user->name = $request->input('name');
         $user->email = $request->input('email');
         $user->password = bcrypt($request->input('password'));
         $user->save();
+
         return 'Modificacion exitosa';
     }
 
-    public function destroy(Request $request){
+    public function destroy(Request $request)
+    {
         $user = User::find($request->input('id'));
         $user->delete();
+
         return 'Usuario eliminado con exito';
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('users',[UserController::class,'index'])->name('users.index');
-Route::get('users/create',[UserController::class,'create'])->name('users.create');
-Route::post('users',[UserController::class,'store'])->name('users.store');
-Route::get('users/{id}',[UserController::class,'show'])->name('users.show');
-Route::get('users/{id}/edit',[UserController::class,'edit'])->name('users.edit');
-Route::put('users/{id}',[UserController::class,'update'])->name('users.update');
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
 //Para la clase delete utilice un formulario en index, donde se pasa el id de la persona que se quiere eliminar
 //La ruta para borrar es /user, no logre pasar los parametros de manera dinamica desde el formulario :(
 Route::delete('/users', [UserController::class, 'destroy'])->name('users.delete');
