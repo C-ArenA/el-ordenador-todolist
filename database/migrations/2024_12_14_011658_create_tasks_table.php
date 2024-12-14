a72435ccdd474a49b6d8c1a94a07f978b06cce27
Cristian Arana <cris171095@gmail.com>
2024-12-14 18:42:26 -0400

    feature: Realiza las migraciones

---
+++ database/migrations/2024_12_14_011658_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status',['por hacer','haciendo','hecho']);
+            $table->enum('priority',['urgente','alta','media','baja']);
+            $table->dateTime('deathline');
+            $table->boolean('notifiable');
+            $table->foreignId('sentence_id');
+            $table->foreignId('team_id');
+            $table->foreignId('project_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
